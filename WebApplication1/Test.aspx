2f1de29548e4dbf74df115c637ef7a6de94c3ef7
Vaibhav Shivgunde <vaibhav.shivgunde7@gmail.com>
2020-06-04 23:33:49 +0530

    Add project files.

---
+++ WebApplication1/Test.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="WebApplication1.Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TreeView ID="TreeView1" ExpandDepth="0" PopulateNodesFromClient="true" ShowLines="true" ShowExpandCollapse="true" runat="server"  OnTreeNodePopulate="TreeView1_TreeNodePopulate" Target="_Blank">
+            </asp:TreeView>
+            <%--<asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl=http://www.slide.com Target="_blank"></asp:HyperLink>--%>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Test.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace WebApplication1
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == false)
+            {
+                PopulateRootLevel();
+            }
+        }
+        private void PopulateRootLevel()
+        {
+            String CS = ConfigurationManager.ConnectionStrings["DBCON"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("select id, title,URL, (select count(*) FROM SampleCategories WHERE parentid=sc.id) childnodecount FROM SampleCategories sc where parentID IS NULL", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                PopulateNodes(dt, TreeView1.Nodes);
+            }
+        }
+        private void PopulateNodes(DataTable dt, TreeNodeCollection nodes)
+        {
+            foreach (DataRow dr in dt.Rows)
+            {
+                TreeNode tn = new TreeNode();
+                tn.Text = dr["title"].ToString();
+                tn.Value = dr["id"].ToString();
+                tn.NavigateUrl = dr["URL"].ToString();
+                nodes.Add(tn);
+                tn.PopulateOnDemand = (Convert.ToInt32(dr["childnodecount"]) > 0);
+            }
+        }
+        protected void TreeView1_TreeNodePopulate(object sender, TreeNodeEventArgs e)
+        {
+            PopulateSubLevel(Convert.ToInt32(e.Node.Value), e.Node);
+        }
+        private void PopulateSubLevel(int parentID, TreeNode parentNode)
+        {
+            String con1 = ConfigurationManager.ConnectionStrings["DBCON"].ConnectionString;
+            String CS = ConfigurationManager.ConnectionStrings["DBCON"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            {
+                SqlCommand cmd = new SqlCommand("select id,title,URL,(select count(*) FROM SampleCategories WHERE parentid=sc.id) childnodecount FROM SampleCategories sc where parentID=@parentID", con);
+                cmd.Parameters.Add("@parentID", parentID);
+                //con.Open();
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                PopulateNodes(dt, parentNode.ChildNodes);
+            }
+        }
+    }
+}
